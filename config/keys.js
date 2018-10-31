a7cee5bd144f7693b16bfc16b766726e84cf044d
Harley Salas <harley@harleysalas.com>
2018-10-31 14:05:40 -0400

    backend

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-  mongoURI: "mongodb://harley:salashs12@ds257808.mlab.com:57808/dev"
+  mongoURI: "mongodb://harley:salashs12@ds257808.mlab.com:57808/dev",
+  secretOrKey: "secret"
 };
